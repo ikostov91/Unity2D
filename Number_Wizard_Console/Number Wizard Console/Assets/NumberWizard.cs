c244ae880204009be0521fe577c390b7334f42aa
Ivaylo <i.kostov91@gmail.com>
2020-01-10 01:14:14 +0200

    First two projects - IntroSetup and NumWizard

---
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    private readonly int MAX = 1000;
+    private readonly int MIN = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Number wizard, yo");
+        Debug.Log("Pick a number.");
+        Debug.Log($"Highest number is: {MAX}");
+        Debug.Log($"Lowest number is: {MIN}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
