a65ef62724017343910faa4b73f8186278d3c78d
Ivaylo <i.kostov91@gmail.com>
2020-01-21 01:41:26 +0200

    Block Breaker WIP

---
+++ Block_Breaker/Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Configuration Parameters
+    [SerializeField] Paddle paddle;
+
+    // State
+    Vector2 paddleToBallVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePosition + paddleToBallVector;
+    }
+}
---
+++ Block_Breaker/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Configuration Parameters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float xMinPosition = 1f;
+    [SerializeField] float xMaxPosition = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get mouse position relative to the set screen size
+        // without having to know if its 1080 width or so on...
+        // This gives position as percentage (from 0 to 1)
+        // 6 is camera size (half of height)
+        // 16 is width (12 height and 4:3 ration => 16)
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * this.screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePosInUnits, this.xMinPosition, this.xMaxPosition);
+        transform.position = paddlePosition;
+    }
+}
