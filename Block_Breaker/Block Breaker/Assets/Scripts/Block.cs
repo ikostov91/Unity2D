bc572f98867e02467b6bfb96983e5c2667fe6f23
Ivaylo <i.kostov91@gmail.com>
2020-01-24 00:50:29 +0200

    Block Breaker WIP

--- Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSoundEffect;
+    [SerializeField] GameObject blockSparklesVFX; 
 
     // Cached reference
     Level level;
+    GameSession gameStatus;
 
     void Start()
     {
         this.level = FindObjectOfType<Level>();
+        this.gameStatus = FindObjectOfType<GameSession>();
         this.level.CountBreakableBlocks();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        this.DestroyBlock();
+        this.TriggerSparklesVFX();
+        this.gameStatus.AddToScore();
     }
 
     private void DestroyBlock()
@@ -25,5 +30,11 @@ public class Block : MonoBehaviour
         AudioSource.PlayClipAtPoint(this.breakSoundEffect, Camera.main.transform.position);
         Destroy(this.gameObject);
         this.level.BlockDestroyed();
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, this.gameObject.transform.position, this.gameObject.transform.rotation);
+        Destroy(sparkles, 1f);
     }
 }
---
+++ Block_Breaker/Block Breaker/Assets/Scripts/GameSession.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameSession : MonoBehaviour
+{
+    // Configuration parameters
+    [Range(0.1f, 1f)] [SerializeField] float gameSpeed = 0.6f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // State variables
+    [SerializeField] int currentScore = 0;
+
+    // Singleton pattern
+    void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else 
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        this.SetScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        this.currentScore += this.pointsPerBlockDestroyed;
+        this.SetScore();
+    }
+
+    private void SetScore()
+    {
+        this.scoreText.text = this.currentScore.ToString();
+    }
+
+    public void RestartGame()
+    {
+        Destroy(gameObject);
+    }
+}
