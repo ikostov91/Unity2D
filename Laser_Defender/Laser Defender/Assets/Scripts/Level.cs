6c6255a8b3d507640094172880a3ca1f23167927
Ivaylo <i.kostov91@gmail.com>
2020-02-05 01:27:16 +0200

    Lase Defender WIP

---
+++ Laser_Defender/Laser Defender/Assets/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        Debug.Log("Load start menu");
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadGameOver()
+    {
+        Debug.Log("Load Game Over");
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
