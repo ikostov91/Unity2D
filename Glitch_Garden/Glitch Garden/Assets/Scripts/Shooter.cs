1fe63a1b386a0393324445a85e038f483ff854f4
Ivaylo <i.kostov91@gmail.com>
2020-02-27 09:07:40 +0200

    Glitch Garden WIP

--- Glitch_Garden/Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch_Garden/Glitch Garden/Assets/Scripts/Shooter.cs
@@ -5,6 +5,54 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject _projectile, _gun;
+    AttackerSpawner _myLaneSpawner;
+
+    private void Start()
+    {
+        this.SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (this.IsAttackerInLane())
+        {
+            Debug.Log("shoot");
+            // TODO change animation state to shooting
+        }
+        else
+        {
+            Debug.Log("sit and wait");
+            // TODO change animation state to idle
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = 
+                (Mathf.Abs(spawner.transform.position.y - this.transform.position.y) <= Mathf.Epsilon);
+            if (isCloseEnough)
+            {
+                this._myLaneSpawner = spawner;
+                // return;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (this._myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     public void Fire()
     {
