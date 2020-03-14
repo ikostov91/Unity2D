e9950b9b8d0fee3a26a06092d365d6acdc678fec
Ivaylo <i.kostov91@gmail.com>
2020-03-14 23:53:46 +0200

    Started TileVania

---
+++ TileVania/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _runSpeed = 5f;
+
+    Rigidbody2D _myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this._myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Run();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // -1 to +1
+        float x = controlThrow * this._runSpeed;
+        float y = this._myRigidBody.velocity.y;
+
+        Vector2 playerVelocity = new Vector2(x, y);
+        this._myRigidBody.velocity = playerVelocity;
+    }
+}
