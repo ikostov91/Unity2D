976d304eea6fc3a1ec62a3552f5da8aff1c33633
ikostov91 <i.kostov91@gmail.com>
2020-03-29 22:49:07 +0300

    TileVania completed. Course Finished.

---
+++ TileVania/Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+    [Tooltip("Game units per second")]
+    [SerializeField] private float _scrollRate = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = 0f;
+        float y = this._scrollRate * Time.deltaTime;
+        Vector2 waterMovement = new Vector2(x, y);
+
+        this.gameObject.transform.Translate(waterMovement);    
+    }
+}
